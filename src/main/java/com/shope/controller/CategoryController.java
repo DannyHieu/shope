e965a7e37ba7b1a1bbc228a36d13fecd9e353ab5
DannyHieu <hieunguyenxuan2012@gmail.com>
2022-10-27 15:46:58 +0700

    category

--- src/main/java/com/shope/controller/CategoryController.java
+++ src/main/java/com/shope/controller/CategoryController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shope.model.Category;
-import com.shope.model.Product;
 import com.shope.service.CategoryService;
 
 @RestController
